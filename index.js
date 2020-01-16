2fd4139eefef8a5eb8c5cf0731237c81b29da465
Joe Ipson <joe@ipson.me>
2020-01-16 11:24:06 -0700

    Added event listener with throttle

--- index.js
+++ index.js
@@ -4,26 +4,46 @@ import React, {
   Dimensions
 } from 'react-native';
 
-const windowSize = Dimensions.get('window');
+// https://learnjswith.me/javascript-throttle-function/
+const throttle = (fn, limit) => {
+  let waiting = false
+  return (...args) => {
+    if (!waiting) {
+      fn.apply(this, args)
+      waiting = true
+      setTimeout(() => {
+        waiting = false
+      }, limit)
+    }
+  }
+};
 
 class DetectDeviceService {
   constructor() {
+    this.checkSizes();
+
+    Dimensions.addEventListener('change', throttle(() => this.checkSizes(), 250));
+  }
+
+  checkSizes() {
+    const windowSize = Dimensions.get('window');
+
     this.pixelDensity = PixelRatio.get();
     this.width = windowSize.width;
     this.height = windowSize.height;
     this.adjustedWidth = this.width * this.pixelDensity;
     this.adjustedHeight = this.height * this.pixelDensity;
-    
+
     this.isPhoneOrTablet();
     this.isIosOrAndroid();
     this.detectIphoneX();
   }
-  
+
   isPhoneOrTablet() {
-    if(this.pixelDensity < 2 && (this.adjustedWidth >= 1000 || this.adjustedHeight >= 1000)) {
+    if (this.pixelDensity < 2 && (this.adjustedWidth >= 1000 || this.adjustedHeight >= 1000)) {
       this.isTablet = true;
       this.isPhone = false;
-    } else if(this.pixelDensity === 2 && (this.adjustedWidth >= 1824 || this.adjustedHeight >= 1824)) {
+    } else if (this.pixelDensity === 2 && (this.adjustedWidth >= 1824 || this.adjustedHeight >= 1824)) {
       this.isTablet = true;
       this.isPhone = false;
     } else {
@@ -31,9 +51,9 @@ class DetectDeviceService {
       this.isPhone = true;
     }
   }
-  
+
   isIosOrAndroid() {
-    if(Platform.OS === 'ios') {
+    if (Platform.OS === 'ios') {
       this.isIos = true;
       this.isAndroid = false;
     } else {
@@ -42,19 +62,20 @@ class DetectDeviceService {
     }
   }
 
-  detectIphoneX(){
-	 if( Platform.OS === 'ios' &&
+  detectIphoneX() {
+    if (
+      Platform.OS === 'ios' &&
       !Platform.isPad &&
       !Platform.isTVOS &&
       (
         (windowSize.height === 812 || windowSize.width === 812) || // iPhone X and iPhone XS
         (windowSize.height === 896 || windowSize.width === 896) // iPhone XS Max and XR
-      )) {
-	 	this.isIphoneX = true;
-	 } else {
-	 	this.isIphoneX = false;
-	 }
-
+      )
+    ) {
+      this.isIphoneX = true;
+    } else {
+      this.isIphoneX = false;
+    }
   }
 }
 
