47c54c23ca927cc3ca4655b55c7e2f6b7e80a040
Joe Ipson <joe@ipson.me>
2020-01-16 11:25:05 -0700

    Fixed formatting

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ class DetectDeviceService {
     this.height = windowSize.height;
     this.adjustedWidth = this.width * this.pixelDensity;
     this.adjustedHeight = this.height * this.pixelDensity;
-    
+
     this.isPhoneOrTablet();
     this.isIosOrAndroid();
     this.detectIphoneX();
   }
-  
+
   isPhoneOrTablet() {
-    if(this.pixelDensity < 2 && (this.adjustedWidth >= 1000 || this.adjustedHeight >= 1000)) {
+    if (this.pixelDensity < 2 && (this.adjustedWidth >= 1000 || this.adjustedHeight >= 1000)) {
       this.isTablet = true;
       this.isPhone = false;
-    } else if(this.pixelDensity === 2 && (this.adjustedWidth >= 1824 || this.adjustedHeight >= 1824)) {
+    } else if (this.pixelDensity === 2 && (this.adjustedWidth >= 1824 || this.adjustedHeight >= 1824)) {
       this.isTablet = true;
       this.isPhone = false;
     } else {
@@ -31,9 +31,9 @@ class DetectDeviceService {
       this.isPhone = true;
     }
   }
-  
+
   isIosOrAndroid() {
-    if(Platform.OS === 'ios') {
+    if (Platform.OS === 'ios') {
       this.isIos = true;
       this.isAndroid = false;
     } else {
@@ -42,18 +42,18 @@ class DetectDeviceService {
     }
   }
 
-  detectIphoneX(){
-	 if( Platform.OS === 'ios' &&
+  detectIphoneX() {
+    if (Platform.OS === 'ios' &&
       !Platform.isPad &&
       !Platform.isTVOS &&
       (
         (windowSize.height === 812 || windowSize.width === 812) || // iPhone X and iPhone XS
         (windowSize.height === 896 || windowSize.width === 896) // iPhone XS Max and XR
       )) {
-	 	this.isIphoneX = true;
-	 } else {
-	 	this.isIphoneX = false;
-	 }
+      this.isIphoneX = true;
+    } else {
+      this.isIphoneX = false;
+    }
 
   }
 }
